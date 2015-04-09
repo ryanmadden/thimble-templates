9f7250947fb2592b624732349b144522f62cff18
Ryan Madden <ryanmadden2017@u.northwestern.edu>
2015-04-08 20:33:51 -0500

    Add piece overlay on hober

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() 
+{
+
+	$('.piece-thumb-container').hover(function() {
+		$overlay = $(this).children(":first");
+		$overlay.fadeTo("fast", 1);
+	}, function() {
+		$overlay = $(this).children(":first");
+		$overlay.fadeTo("fast", 0);
+	});
+
+});
 
